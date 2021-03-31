277ff722d5e7d9e798a9a7f553442a4a7e14a66a
Stephan Kochauf <stephan.kochauf@lv.campus02.at>
2021-03-31 21:09:48 +0200

    transactions

---
+++ src/org/campus02/transactions/TransactionDemoApp.java
@@ -0,0 +1,10 @@
+package org.campus02.transactions;
+
+public class TransactionDemoApp {
+    public static void main(String[] args) {
+
+        String path = "D:\\temp\\transactions.csv";
+
+
+    }
+}
---
+++ src/org/campus02/transactions/TransactionLoader.java
@@ -0,0 +1,10 @@
+package org.campus02.transactions;
+
+import java.util.ArrayList;
+
+public class TransactionLoader {
+    public static ArrayList<Transaction> loadTransactions(String path) {
+
+        return null;
+    }
+}
